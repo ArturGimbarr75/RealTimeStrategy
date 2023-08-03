93dc1158b3a016766f6ebeca32e9c1abc46873d3
ArturCodeAcademy <123232198+ArturCodeAcademy@users.noreply.github.com>
2023-08-03 21:47:59 +0300

    SpawnSystem

---
+++ Assets/Scripts/Buildings/UnitsSpawner.cs
@@ -0,0 +1,41 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UnitsSpawner : NetworkBehaviour, IPointerClickHandler
+{
+    [SerializeField]
+    private GameObject _unitPrefab;
+    [SerializeField]
+    private Transform _unitSpawnPoint;
+
+    #region Server
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        GameObject unitInstance = Instantiate(  _unitPrefab,
+                                                _unitSpawnPoint.position,
+                                                _unitSpawnPoint.rotation);
+
+        NetworkServer.Spawn(unitInstance, connectionToClient);
+    }
+
+    #endregion
+
+    #region Client
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Left)
+            return;
+        if (!isOwned)
+            return;
+
+        CmdSpawnUnit();
+    }
+
+    #endregion
+}
--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -5,5 +5,16 @@ using UnityEngine;
 
 public class RTSNetworkManager : NetworkManager
 {
-    
+    [Header(nameof(RTSNetworkManager))]
+    [SerializeField] private GameObject _unitSpawnerPrefab = null;
+
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        GameObject unitSpawnerInstance = Instantiate(_unitSpawnerPrefab,
+                                                     conn.identity.transform.position,
+                                                     conn.identity.transform.rotation);
+        NetworkServer.Spawn(unitSpawnerInstance, conn);
+    }
 }
