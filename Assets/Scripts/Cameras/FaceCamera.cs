4f35f4ff19f979937b4396fe49999802eaf2586a
ArturCodeAcademy <123232198+ArturCodeAcademy@users.noreply.github.com>
2023-09-04 00:41:20 +0300

    UI facing

---
+++ Assets/Scripts/Cameras/FaceCamera.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private void LateUpdate()
+    {
+        transform.LookAt(
+            transform.position + Camera.main.transform.rotation * Vector3.forward,
+            Camera.main.transform.rotation * Vector3.up);
+    }
+}
